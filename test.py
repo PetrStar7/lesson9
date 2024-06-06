d1e9c248001f4b31fb909c93ce73eb629ede6e18
Starda <mgr.stary@gmail.com>
2024-06-06 21:09:53 +0200

    Opraveno dle požadavků zadavatele.

---
+++ test.py
@@ -0,0 +1,8 @@
+x = input("Zadej první číslo:")
+y = input("Zadej druhé číslo:")
+z = input("Zadej třetí číslo:")
+a = int(x)
+b = int(y)
+c = int(z)
+print("Součet čísel je:", a + b + c)
+print("Součin čísel je:", a * b * c)
